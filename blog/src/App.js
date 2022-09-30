b598545650f55230ddf613bc37e5f436e7132a14
77r77ung <ocao1104@gmail.com>
2022-09-30 13:32:10 +0900

    [React_Project_1] React Blog | ~ 22.09.28

--- blog/src/App.js
+++ blog/src/App.js
@@ -19,10 +19,16 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학'])
 
   {/* 따봉: 실제 State | 따봉변경: State 변경 함수(state 변경 함수를 사용해야 html 재렌더링도 잘 됨) */}
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState(글제목.map(function(a){
+    return 0
+  }));
+  {/* 글제목만큼 따봉 State 생성 -> [0, 0, 0] */}
+
   function 함수(){
     console.log(1);
   }
+
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App"> {/* html이 아니라 JSX(.js 파일에서 쓰는 html 대용품) 문법임 */}
@@ -41,16 +47,17 @@ function App() {
         글 제목 가나다순 정렬하기!
       </button>
 
+      {/* 
       <div className="list">
-        { /* onClick = {} 안에는 함수 이름만 넣어주어야 함 */}
-        { /* 문자도 동일함 1만 '반가워'로 변경해주면 됨 */}
+        onClick = {} 안에는 함수 이름만 넣어주어야 함
+        /* 문자도 동일함 1만 '반가워'로 변경해주면 됨
         <h4> { 글제목[0] } <span onClick = {()=> { 따봉변경(따봉+1) }}> 👍 </span> { 따봉 } </h4>
-        { /* 이렇게 함수 만드는 문법을 바로 어줘도 상관없음! (세개 모두 동일한 코드)
+        이렇게 함수 만드는 문법을 바로 어줘도 상관없음! (세개 모두 동일한 코드)
         span onClick = { function(){ console.log(1) } } 
-        span onClick = { () => { console.log(1)} } */}
+        span onClick = { () => { console.log(1)} }
         <p> 2월 17일 발행 </p>
 
-        {/* 숙제 2: 버튼 누르면 첫번째 게시물 제목이 '여자코트 추천'으로 변경되게 하기 */}
+        숙제 2: 버튼 누르면 첫번째 게시물 제목이 '여자코트 추천'으로 변경되게 하기
         <button onClick = {()=> { 글제목변경(['여자코트 추천', '강남 우동맛집', '파이썬 독학']) }}> 숙제버튼 </button>
         {/* 이렇게도 가능합니당.
         <button onClick = {()=> {
@@ -63,36 +70,80 @@ function App() {
           copy[0] = '여자코트 추천'
           글제목변경(copy);
         }
-        */}
       </div>
-
-      {/* 하지만, 컴포넌트는 state를 가져다쓸 때 문제가 된다. */}
+      
       <div className="list">
         <h4> { 글제목[1] }</h4>
         <p> 2월 17일 발행 </p>
       </div>
-
+      
+      숙제4: 제목 한 번 누르면 Modal창 띄우기, 한 번 더 누르면 Modal창 가리기
       <div className="list">
-        <h4> { 글제목[2] }</h4>
+        <h4 onClick={()=>{ 
+          modal == false ? setModal(true) : setModal(false)
+         }}> { 글제목[2] }</h4>
         <p> 2월 17일 발행 </p>
       </div>
       
-      {/* <Modal/> 로도 사용 가능함 */}
-      <Modal></Modal>
+      <Modal/> 로도 사용 가능함
+
+      동적 UI(ex. 탭, 모달창, 툴팁, 경고문 등) -> 
+        1. html, CSS로 미리 디자인 완성
+        2. UI의 현재 상태 State로 미리 저장해두기
+        3. State의 상태에 따라 UI가 어떻게 보일지 작성
+          -> 제목을 누를 때마다 모달 창 기능이 펼쳐지도록 구현해보기
+      html에는 if문이 적용되지 않으므로 삼항연산자를 if문으로 사용
+          null: 비어있는 html 용도로 자주 사용
+      {
+        modal == true ? <Modal/> : null
+      }
+      */}
+
+      {/* Map 함수를 이용해 html 반복하기
+          숙제5: Map 함수를 이용해 글을 띄우고 글마다 개별적인 좋아요 갯수 기록하기 */}
+      {
+        글제목.map(function(a, i){
+          return (
+            <div className="list" key = {i} >
+              <h4 onClick = { ()=> {setModal(!modal)} } > {글제목[i]} </h4>
+              <span onClick = {()=> { 
+                let copy = [...따봉];
+                copy[i] = 따봉[i]+1;
+                따봉변경(copy);
+               }}> 👍 </span> { 따봉[i] }
+              <p> 2월 17일 발행 </p>
+            </div>
+          )
+        })
+        // 위와  동일코드
+        // 글제목.map(function(a){
+        //   return (
+        //     <div className="list">
+        //       <h4> { a }</h4>
+        //       <p> 2월 17일 발행 </p>
+        //     </div>
+        //   )
+        // }) 
+      }
+      {
+        modal == true ? <Modal 글제목 = {글제목} 글제목변경 = {글제목변경}/> : null
+      }
 
     </div>
   );
 }
 
-{/* 컴포넌트 만들기 */}
-function Modal(){
-  return (
-    <div className = "modal">
-        <h4> 제목 </h4>
-        <p> 날짜 </p> 
-        <p> 상세 내용 </p>
-    </div>
-  )
+{/* 컴포넌트 만들기
+    숙제 6: 글 수정 버튼을 누르면 첫 글 제목이 '여자코트 추천'으로 변경되야 함
+    보너스 숙제: Modal 안의 글 제목과 글 목록의 글 제목 일치시키기 */}
+function Modal(props){
+  let [copy_글제목, onModal] = useState(props.글제목.map(function(a, i){
+    return(
+      <div className="modal" style={{background : props.color}} key={i}>
+        <h4> {a[i]} </h4>
+      </div>
+    )
+  })) 
 }
 
 export default App;

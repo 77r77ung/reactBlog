2fa4142b49ab73cdd3aa8961c71f3c133e55cb2a
77r77ung <ocao1104@gmail.com>
2022-10-05 18:31:52 +0900

    [React] ReactBlog_mineBlog | For Uploading a Velog

---
+++ mineblog/mineblog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="nav">
+      <h4 style={ {color:'white', fontsize:'16px'} }> nav입니다. </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
